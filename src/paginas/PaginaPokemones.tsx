a6eef48196e259e08b86b5bf9498f78d20de8ab3
0loke0 <82248687+0loke0@users.noreply.github.com>
2022-01-17 16:38:57 -0500

    a
    a

---
+++ src/paginas/PaginaPokemones.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+interface tipos {
+  IdTipo: number;
+  NombreTipo: string;
+}
+
+function PaginaPokemones() {
+  const [tipos, settipos] = useState<tipos[]>();
+  useEffect(() => {
+    fetch("http://localhost:63107/api/PruebasPoke/InformacionDePruebas2")
+      .then((response) => response.json())
+      .then((data) => settipos(data));
+  }, []);
+  return (
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Tipo</th>
+      </tr>
+
+      {tipos.map((x) => (
+        <tr>
+          <td>{x.IdTipo}</td>
+          <td>{x.NombreTipo}</td>
+        </tr>
+      ))}
+    </table>
+  );
+}
+
+export default PaginaPokemones;
