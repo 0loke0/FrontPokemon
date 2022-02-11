e6cc401369b47911e09e17996896a2d4f99bd831
0loke0 <82248687+0loke0@users.noreply.github.com>
2022-02-11 18:42:21 -0500

    A
    A

--- src/paginas/PaginaPokemones/Secciones/SCard.tsx
+++ src/paginas/PaginaPokemones/Secciones/SCard.tsx
@@ -8,6 +8,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import styled from "styled-components";
 import { keyframes } from "styled-components";
+import { Row } from "react-bootstrap";
 
 interface IPropSCard {
   pokemon: IPokemon;
@@ -17,13 +18,68 @@ interface IRelacionTipoPokemon {
   Nombre: string;
   NombreTipo: string;
 }
+// const SCardContenedor = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   background-color: #ffffff;
+//   border-radius: 5%;
+//   margin: 4%;
+//   border: 2px solid #8cd4f5;
+//   padding: 10px;
+//   box-shadow: 5px 5px 10px #a8a4a4;
+// `;
+
+const SDivTitulo = styled.div`
+  text-align: right;
+  margin: 2%;
+  font-family: monospace;
+  font-size: x-large;
+`;
+
+const SCol = styled(Col)`
+  margin: 1% 4%;
+  width: 20%;
+  height: 40px;
+  text-align: center;
+  border: 1px solid #29c5f1;
+  border-radius: 10%;
+`;
+const SContenedorImagen = styled.div`
+  position: relative;
+  top: 0%;
+  left: 50%;
+  width: 95%;
+  height: 200px;
+  background-color: #82d1d6;
+  border-radius: 5%;
+  border: 2px Solid black;
+  transform: translate(-50%, 0%);
+`;
+
+const SContenedorTipo1 = styled.div`
+  position: relative;
+  width: 30%;
+  height: 15%;
+  background-color: #82d1d6;
+  border-radius: 30%;
+  border: 2px Solid black;
+  transform: translate(-25%, -50%);
+`;
+const SContenedorTipo2 = styled.div`
+  position: relative;
+  width: 30%;
+  height: 15%;
+  background-color: #82d1d6;
+  border-radius: 30%;
+  border: 2px Solid black;
+  transform: translate(80%, -150%);
+`;
 
 const StyledCard = styled(Card)`
   padding: 5px;
   border-radius: 10px;
   border: 2px solid #9dccfa;
   box-shadow: 5px 5px 10px #7d7d7d;
-  
 `;
 
 export const SCard: FC<IPropSCard> = ({ pokemon }) => {
@@ -39,6 +95,25 @@ export const SCard: FC<IPropSCard> = ({ pokemon }) => {
     <Col>
       <StyledCard>
         <Card.Body>
+          {/* <>
+            <SDivTitulo>Titulo</SDivTitulo>
+            <SContenedorImagen>
+              <SContenedorTipo1></SContenedorTipo1>
+              <SContenedorTipo2></SContenedorTipo2>
+            </SContenedorImagen>
+            <Row>
+              <SCol>info</SCol>
+              <SCol>info</SCol>
+            </Row>
+            <Row>
+              <SCol>info</SCol>
+              <SCol>info</SCol>
+            </Row>
+            <Row>
+              <SCol>info</SCol>
+              <SCol>info</SCol>
+            </Row>
+          </> */}
           <Card.Title>{pokemon.Nombre}</Card.Title>
           <Card.Text>Id: {stat?.IdPokemon}</Card.Text>
           <Card.Text>Defensa: {stat?.Defensa}</Card.Text>
