302f49543dc499eb5840f2c07e472f3dbd9ee9c3
0loke0 <82248687+0loke0@users.noreply.github.com>
2022-03-15 11:15:12 -0500

    Update ContenedorCards.tsx

--- src/paginas/PaginaPokemones/Secciones/Card/ContenedorCards.tsx
+++ src/paginas/PaginaPokemones/Secciones/Card/ContenedorCards.tsx
@@ -90,6 +90,7 @@ export const ContenedorCards: FC<IPropCardPokemon> = ({ pokemon }) => {
 
   return (
     <>
+      {console.log(PokemonDetallado)}
       <Row xs={1} md={3} className='g-4'>
         {PokemonDetallado ? (
           PokemonDetallado.map((data, index) => {
