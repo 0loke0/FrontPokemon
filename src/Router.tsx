6fd578a304a4fd7032af2870aad77f1b562b2910
0loke0 <82248687+0loke0@users.noreply.github.com>
2022-05-20 09:48:18 -0500

    borado
    borrado rutas no usadas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,40 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PaginasPokemon from "./paginas/PaginaPokemones/PaginaPokemones";
-import PaginaStats from "./paginas/PaginaStats/PaginaStats";
-import LoadingDian from "./paginas/LoadingDian/LoadingDian";
-import Cronometro from "./paginas/Cronometro/Cronometro";
-import CardGeneralizado from "./paginas/PruebasCards/CardGeneralizado";
-import { MarcaDeAgua } from "./paginas/MarcaDeAgua/MarcaDeAgua";
-import DragAndDrop from "./paginas/PruebasDragAndDrop/DragAndDrop";
-import PruebasStats from "./paginas/PruebasStats/PruebasStats";
+
 export const Rutas = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
           <PaginasPokemon />
-        </Route>
-        <Route path='/Stats'>
-          <PaginaStats />
-        </Route>
-        <Route path='/LoadingDian'>
-          <LoadingDian />
-        </Route>
-        <Route path='/Cronometro'>
-          <Cronometro />
-        </Route>
-        <Route path='/CardPruebas'>
-          <CardGeneralizado />
-        </Route>
-        <Route path='/MarcaDeAgua'>
-          <MarcaDeAgua />
-        </Route>
-        <Route path='/DragAndDrop'>
-          <DragAndDrop />
-        </Route>
-        <Route path='/PruebasStats'>
-          <PruebasStats />
         </Route>
       </Switch>
     </Router>
